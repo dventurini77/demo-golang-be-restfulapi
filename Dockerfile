19721854daa9dedd8b748b3ca6869169c44503ad
Davide Venturini <davven_it@yahoo.it>
2020-05-18 11:15:37 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Start from the latest golang base image
+FROM golang:latest
+
+# Add Maintainer Info
+LABEL maintainer="Davide Venturini <davide.venturini@silfi.it>"
+
+# Set the Current Working Directory inside the container
+WORKDIR /app
+
+RUN go get -u github.com/gorilla/mux
+RUN go get -u github.com/jinzhu/gorm
+RUN go get -u github.com/lib/pq
+
+# Copy the source from the current directory to the Working Directory inside the container
+COPY /src/ .
+
+# Build the Go app
+RUN go build -o main .
+
+# Expose port 8080 to the outside world
+EXPOSE 8080
+USER 1001
+
+# Command to run the executable
+CMD ["./main"]
---
+++ src/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// User model
+type User struct {
+	ID    int `gorm: "primary_key";"AUTO_INCREMENT"`
+	Name  string
+	Email string
+}
+
+var psqlInfo string
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var params = mux.Vars(r) // Get params
+	var user User
+
+	db.Debug().Where("ID = ?", params["id"]).First(&user)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&user)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	db.Debug().Create(&user)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&user)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var params = mux.Vars(r) // Get params
+	var upduser, user User
+	_ = json.NewDecoder(r.Body).Decode(&upduser)
+
+	db.Debug().Where("ID = ?", params["id"]).First(&user).Update(&upduser)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var params = mux.Vars(r) // Get params
+	var user User
+
+	db.Debug().Where("ID = ?", params["id"]).Delete(&user)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&user)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/api/users", getUsers).Methods("GET")
+	myRouter.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+	myRouter.HandleFunc("/api/users", createUser).Methods("POST")
+	myRouter.HandleFunc("/api/users/{id}", updateUser).Methods("PUT")
+	myRouter.HandleFunc("/api/users/{id}", deleteUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func initialMigration() {
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
+	db.LogMode(true)
+	db.SetLogger(log.New(os.Stdout, "\r\n", 0))
+}
+
+func initialDbConnection() {
+	var host = os.Getenv("PGDATABASE_HOST")
+	var port = os.Getenv("PGDATABASE_PORT")
+	var user = os.Getenv("PGDATABASE_USER")
+	var password = os.Getenv("PGDATABASE_PASS")
+	var dbname = os.Getenv("PGDATABASE_NAME")
+
+	psqlInfo = fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	log.Println(psqlInfo)
+}
+
+func main() {
+	log.Println("Service RESTful::User started")
+
+	initialDbConnection()
+
+	initialMigration()
+
+	// Handle Subsequent requests
+	handleRequests()
+}
